219e246aa9c30dea575a6f911e42aa91c97330ba
Tammi L. Coles <21356324+tlcoles@users.noreply.github.com>
2022-10-25 20:46:55 +0200

    Add comments and change put to post

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -7,13 +7,15 @@ const initdb = async () =>
         console.log('jate database already exists');
         return;
       }
+
+      // Create a new object store for the data and give it a key name of 'id' which needs to increment automatically.
       db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
       console.log('jate database created');
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => {
+// DONE: Add logic to a method that accepts some content and adds it to the database
+export const postDb = async (content) => {
   console.log('Post to the database');
 
   // Create a connection to the database database and version we want to use.
@@ -26,14 +28,14 @@ export const putDb = async (content) => {
   const store = tx.objectStore('jate');
 
   // Use the .add() method on the store and pass in the content.
-  const request = store.add( {content } );
+  const request = store.add( { content } );
 
   // Get confirmation of the request.
   const result = await request;
   console.log('🚀 - data saved to the database', result);
 };
 
-// TODO: Add logic for a method that gets all the content from the database
+// DONE: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
   console.log('GET from the database');
 
